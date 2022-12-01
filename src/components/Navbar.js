a11ecba4ad6e048426ea67f359d0428a26c2dff4
preciousimo <preciousimoniakemu@gmail.com>
2022-12-01 10:54:55 +0100

    Middle side (search) of navbar 🔍

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,22 +11,25 @@ export default function Navbar() {
         <div className='flex items-center justify-between px-5 py-3'>
             {/* Left Side */}
             <div className='flex items-center shrink-0'>
-                <FiMenu className='w-6 h-6' />
+                <FiMenu className='w-6 h-6'/>
                 <div className='pl-4'>
-                    <img src={logo} alt="logo" className='w-32' />
+                    <img src={logo} alt="logo" className='w-32'/>
                 </div>
             </div>
 
             {/* Middle */}
+            <div className='hidden sm:flex w-full'>
+                <input type="search" placeholder="Search..." className="outline-0 bg-[#dae0e6] rounded-full pl-4 py-2 w-full mx-10"/>
+            </div>
             
             {/* Right Side */}
             <div className='flex h-full items-center gap-3'>
-                <FiSearch className='h-6 w-6' />
-                <GrAppsRounded className='h-6 w-6' />
-                <IoIosSettings className='h-6 w-6' />
+                <FiSearch className='h-6 w-6'/>
+                <GrAppsRounded className='h-6 w-6'/>
+                <IoIosSettings className='h-6 w-6'/>
                 <div className='flex items-center bg-[#a359a0] p-1 rounded-full text-white'>
-                    <FaUser className='h-5 w-5 ml-1.5 mr-1' />
-                    <p className='font-bold mr-1.5'>Log in</p>
+                    <FaUser className='h-5 w-5 ml-1.5 mr-1'/>
+                    <p className='font-bold mr-1.5 whitespace-nowrap'>Log in</p>
                 </div>
             </div>
         </div>
