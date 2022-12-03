9082bb7a3c2c06b0fe43cd15bc0ec856c01b2537
preciousimo <preciousimoniakemu@gmail.com>
2022-12-03 13:08:10 +0100

    Style sidebar and topic buttons ▶️

--- src/components/SideBtn.jsx
+++ src/components/SideBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SideBtn({ name, icon }) {
   return (
     <div className='flex flex-col lg:flex-row items-center hover:bg-gray-200 w-full py-1 rounded-[0.5rem] 
-    duration-200 ease-out lg:gap-4'>
+    duration-200 ease-out lg:gap-4 md:py-3 md:px-3'>
         <div className='text-[20px] lg:text-[28px]'>{icon}</div>
         <p className='text-[10px] lg:text-[17px]'>{name}</p>
     </div>
--- src/components/TopicBtn.jsx
+++ src/components/TopicBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TopicBtn({ topic }) {
     return (
-        <div className='bg-gray-100 rounded-full'>
+        <div className='bg-gray-100 hover:bg-gray-400 hover:text-white rounded-full'>
             <p className='py-1 px-3'>{topic}</p>
         </div>
     );
