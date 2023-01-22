e53a55cb66a2a712c2314e944fdd1958ee62b54a
preciousimo <preciousimoniakemu@gmail.com>
2023-01-22 14:54:10 +0100

    Create signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import LoginPage from './pages/Authenticate/LoginPage' 
+import SignupPage from './pages/Authenticate/SignupPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Home/>} path="" />
         <Route element={<LoginPage/>} path="/login" />
+        <Route element={<SignupPage/>} path="/signup" /> 
       </Routes>
     </Router>
   );
--- src/pages/Authenticate/SignupPage.jsx
+++ src/pages/Authenticate/SignupPage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MdOutlineCancel } from "react-icons/md";
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
     <div className="bg-purple-500 min-h-screen flex items-center justify-center">
       <form className="bg-white p-6 rounded-lg shadow-md">
         <div className="flex justify-between">
-          <h2 className="text-lg font-medium mb-4">Login</h2>
+          <h2 className="text-lg font-medium mb-4">Sign Up</h2>
           <Link to={"/"}>
             <MdOutlineCancel className="" />
           </Link>
@@ -30,12 +30,25 @@ const LoginPage = () => {
             placeholder="Enter your password"
           />
         </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">
+            Confirm Password
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded-lg w-full"
+            type="password"
+            placeholder="Confirm your password"
+          />
+        </div>
         <div className="flex justify-between">
           <button className="bg-[#a359a0] rounded-full text-white py-2 px-4 hover:bg-purple-700">
-            Login
+            Sign Up
           </button>
-          <Link className="bg-[#a359a0] rounded-full text-white py-2 px-4 hover:bg-purple-700" to={"/signup"}>
-            Signup
+          <Link
+            className="bg-[#a359a0] rounded-full text-white py-2 px-4 hover:bg-purple-700"
+            to={"/login"}
+          >
+            Login
           </Link>
         </div>
       </form>
@@ -43,4 +56,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
