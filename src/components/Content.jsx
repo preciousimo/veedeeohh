6138a3614ab30cecd8caf9a38cc132fe3d7025c1
preciousimo <preciousimoniakemu@gmail.com>
2023-03-14 21:47:39 +0100

    Fetch content video

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -21,8 +21,9 @@ export default function Content() {
             {contents.map((content) => (
                 <div className="mx-12 sm:mx-0">
                     <div className="flex h-48 justify-center shadow-gray-300 rounded-[0.7rem] md:h-32 lg:h-48 xxl:h-60 overflow-hidden"> {/* include "overflow-hidden" to classname*/}
-                        <img src={content.video} alt="Thumbmail"
-                        className="object-cover w-full h-full rounded-[0.7rem] md:h-32 lg:h-full hover:scale-110 duration-200 ease-out" />
+                        <video src={content.video} alt="Thumbmail" width="250" height="250"
+                        className="object-cover   rounded-[0.7rem] md:h-32 lg:h-full hover:scale-110 duration-200 ease-out" controls>
+                        </video>
                     </div>
                     <div className="flex items-center pt-2">
                         <div className="w-10 h-10 shrink-0">
